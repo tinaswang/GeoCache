6bcf8aaa9cd9f16423cd78ff13a4e42f0e3dae11
Tina Wang <tswang@caltech.edu>
2017-10-26 10:44:53 -0700

    UI changes

--- GeoCache/GeoCache.swift
+++ GeoCache/GeoCache.swift
@@ -1,9 +1,110 @@
 //
 //  GeoCache.swift
-//  GeoCache
-//
+//  GeoCache is a struct to keep track of geocache
+//  properties and other information.
 //  Created by Tina Wang on 10/22/17.
-//  Copyright © 2017 Tina Wang. All rights reserved.
+//  
 //
 
 import Foundation
+
+struct GeoCache {
+    // Variables for all the textfields
+    var title: String
+    var details: String
+    var creator: String
+    var reward: String
+
+    //  A failable initializer which takes a dictionary, with values for the
+    //    keys “title”, “details”, “creator”, and “reward”.
+    //    If any of these is missing, the initializer returns nil.
+    init?(fromDictionary dict: [String: String]) {
+        // Check whether each value is present
+        if let value : String = dict["title"] {
+            self.title = value
+        }
+        else {
+            return nil
+        }
+        
+        if let value: String = dict["details"] {
+            self.details = value
+        }
+        
+        else {
+            return nil
+        }
+        if let value : String = dict["creator"] {
+            self.creator = value
+        }
+        else {
+            return nil
+        }
+        
+        if let value: String = dict["reward"] {
+            self.reward = value
+        }
+            
+        else {
+            return nil
+        }
+    }
+    
+    //  dictionary: a computed property which returns a dictionary using the GeoCache information
+    var dictionary: [String: String] {
+        get {
+            var geo_dict = [String: String]()
+            geo_dict["title"] =  self.title
+            geo_dict["details"] =  self.details
+            geo_dict["creator"] =  self.creator
+            geo_dict["reward"] =  self.reward
+            return geo_dict
+            
+        }
+
+    }
+    
+    // Prints out the GeoCache information.
+    
+    var description: String {
+        get {
+            return "\(title): \n Located at \(details) " +
+                    "with reward \(reward).\nCreated by \(creator)."
+        }
+    }
+}
+
+    //  Look up an array in UserDefaults and attempt to convert each element 
+    // in the array into a dictionary of type [String: String].
+    // Feed those into the initializer for GeoCache, 
+    // and return an array of those results.
+
+    func loadCachesFromDefaults() -> [GeoCache] {
+        let defaults = UserDefaults.standard
+        var result = [GeoCache]()
+        // Feed in each GeoCache into the failable initializer
+        if let cache_info = defaults.array(forKey: "cache_info") {
+            for i in cache_info {
+                let i_dict = i as! [String: String]
+                result.append(GeoCache(fromDictionary: i_dict)!)
+            }
+        }
+        else {
+            result = []
+        }
+        
+        return result
+    }
+    //  saveCachesToDefaults: takes an array of GeoCaches and saves it 
+    // to the standard UserDefaults under the key name "cache_info."
+    func saveCachesToDefaults(_ caches: [GeoCache]) {
+        var cache_info = [[String: String]]()
+        for i in caches {
+            cache_info.append(i.dictionary)
+        }
+        let defaults = UserDefaults.standard
+        defaults.set(cache_info, forKey: "cache_info")
+        defaults.synchronize()
+    }
+
+
--- GeoCache/ViewController.swift
+++ GeoCache/ViewController.swift
@@ -2,17 +2,77 @@
 //  ViewController.swift
 //  GeoCache
 //
-//  Created by Tina Wang on 10/22/17.
-//  Copyright © 2017 Tina Wang. All rights reserved.
+// Created by Tina Wang on 10/22/17.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    // Create a GeoCache array
+    var geocache_arr =  [GeoCache]()
+    // Create a counter variable for cycling through GeoCaches
+    var counter : Int = 0
+    // IBOOutlet for the text fields
 
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var detailField: UITextField!
+    @IBOutlet weak var creatorField: UITextField!
+    @IBOutlet weak var rewardField: UITextField!
+    @IBOutlet weak var cacheField: UILabel!
+    
+    // Take whatever’s entered in the four text fields,
+    // and use them to create a new GeoCache.
+    // Print an error message if any of these fields are empty.
+    // Add the new GeoCache to the list of caches, and save the list
+    // of caches to the UserDefaults.
+
+    @IBAction func addGeoCache(_ sender: UIButton) {
+        var geo_dict = [String: String]()
+        if let value : String = titleField.text {
+           geo_dict["title"] =  value
+        }
+        else {
+            cacheField.text = "Error: No title given."
+        }
+        
+        if let value : String = detailField.text {
+            geo_dict["details"] =  value
+        }
+        else {
+            cacheField.text = "Error: No details given."
+        }
+        if let value : String = creatorField.text {
+            geo_dict["creator"] =  value
+        }
+        else {
+            cacheField.text = "Error: No creator given."
+        }
+        if let value : String = rewardField.text {
+            geo_dict["reward"] =  value
+        }
+        else {
+            cacheField.text = "Error: No reward given."
+        }
+
+        geocache_arr.append(GeoCache(fromDictionary: geo_dict)!)
+    }
+    
+    // Cycle the cache displayed in the cacheLabel, 
+    // and setting the cacheLabel to the description property of that GeoCache
+    @IBAction func nextCache(_ sender: UIButton) {
+        cacheField.text = self.geocache_arr[counter].description
+        if counter < self.geocache_arr.count {
+            counter += 1
+        }
+        else {
+            counter = 0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // load the saved GeoCaches from the UserDefaults when the app is started
+        self.geocache_arr = loadCachesFromDefaults()
     }
 
     override func didReceiveMemoryWarning() {
